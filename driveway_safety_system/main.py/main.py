dbcb61f73a85615f4989660d536523f8dc89e3ea
Rishi Israni <rishiisrani07@gmail.com>
2026-05-18 15:35:44 -0700

    Implementation of Real Time Object/Pedestrian Detection

--- driveway_safety_system/main.py/main.py
+++ driveway_safety_system/main.py/main.py
@@ -1,21 +1,70 @@
 import cv2
+from ultralytics import YOLO
 
-camera = cv2.VideoCapture(0)
+model = YOLO("yolov8n.pt")
+
+camera = cv2.VideoCapture(1)
 
 while True:
+
     success, frame = camera.read()
+    vehicle_detected = False
+    results = model(frame)
 
-    cv2.putText(
-        frame,
-        "Monitoring Driveway",
-        (50, 50),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 255, 0),
-        2
-    )
+    for result in results:
 
-    cv2.imshow("Driveway Camera", frame)
+        for box in result.boxes:
+
+            class_id = int(box.cls[0])
+            class_name = model.names[class_id]
+
+            if class_name in [ "car", "truck", "bus", "motorcycle", "person", "bicycle"]:
+
+                vehicle_detected = True
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cv2.rectangle(
+                    frame,
+                    (x1, y1),
+                    (x2, y2),
+                    (0,255,0),
+                    2
+                )
+
+                cv2.putText(
+                    frame,
+                    class_name,
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.8,
+                    (0,255,0),
+                    2
+                )
+
+    if vehicle_detected:
+
+        cv2.putText(
+            frame,
+            "UNSAFE TO REVERSE",
+            (50, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,0,255),
+            3
+        )
+
+    else:
+
+        cv2.putText(
+            frame,
+            "SAFE",
+            (50, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,255,0),
+            3
+        )
+
+    cv2.imshow("Driveway Vehicle Detection", frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
